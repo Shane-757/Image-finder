9efa01935d55de6b4dce974b217769e61b885fa1
Shane-757 <underwoodshane@ymail.com>
2023-05-11 14:12:29 -0400

    update

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -4,15 +4,15 @@ import '../styles.css';
 
 const Loader = () => {
   return (
-    <Dna
-      height={80}
-      width={80}
-      radius={9}
-      color="green"
-      ariaLabel="loading"
-      wrapperStyle={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}
-      wrapperClassName="loader-wrapper"
-    />
+    <div className="loader-wrapper">
+      <Dna
+        height={80}
+        width={80}
+        radius={9}
+        color="green"
+        ariaLabel="loading"
+      />
+    </div>
   );
 };
 
